8757a149f60d4db102befb16e3be709973fb8382
Suzie Joe <szjoe89@naver.com>
2020-01-11 21:20:49 +0900

    detail web

--- web.js
+++ web.js
@@ -1,10 +1,41 @@
-var detailImage = document.querySelector(".detailImage");
 
 function detail_link(detail_link) {
-    console.log(detailImage);
-    if (detail_link.id == "detail_link_1") {
-        detailImage.src = "images/detail.jpg";
+    window.location.href = "detail.html?index="+detail_link.id; 
+}
 
+
+window.onload = function() {
+    var detailImage = document.querySelector(".detailImage");
+ 
+    var val = location.href.substr(
+        location.href.lastIndexOf('=') + 1
+    );
+    if(val == "detail_link_1"){
+        detailImage.src = "images/detail.jpg"
     }
-    location.href = "detail.html";
+    else if(val == "detail_link_2"){
+        detailImage.src = ""
+    }
+    else if(val == "detail_link_3"){
+        detailImage.src = ""
+    }
+    else if(val == "detail_link_4"){
+        detailImage.src = ""
+    }
+    else if(val == "detail_link_5"){
+        detailImage.src = ""
+    }
+    else if(val == "detail_link_6"){
+        detailImage.src = ""
+    }
+    else if(val == "detail_link_7"){
+        detailImage.src = ""
+    }
+    else if(val == "detail_link_8"){
+        detailImage.src = ""
+    }
+    else if(val == "detail_link_9"){
+        detailImage.src = ""
+    }
+        
 }
